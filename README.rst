d298b05bc88f2211672af3ca4cd8a8e410b9bc4d
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-15 15:35:12 +0200

    solid angle package

---
+++ README.rst
@@ -0,0 +1,8 @@
+Solid Angle Utils
+=================
+|BlackStyle|
+
+A collection of tools to help with solid angles.
+
+.. |BlackStyle| image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :target: https://github.com/psf/black
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="solid_angle_utils_sebastian-achim-mueller",
+    version="0.0.1",
+    description="Helps you with solid angles and cones.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/cherenkov-plenoscope/solid_angle_utils",
+    project_urls={
+        "Bug Tracker": "https://github.com/cherenkov-plenoscope/solid_angle_utils/issues",
+    },
+    author="Sebastian Achim Mueller",
+    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
+    packages=["solid_angle_utils",],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+    ],
+)
